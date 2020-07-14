963374fddf77cdef80a2f72a82742daf77744680
ayush219 <52164261+ayush219@users.noreply.github.com>
2020-07-14 13:04:36 +0530

    Add files via upload

---
+++ Logistic Regression/Classification_Template/My_classification_template.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul 13 22:03:18 2019
+
+@author: Ayush
+"""
+#Libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+#Dataset
+dataset= pd.read_csv('Social_Network_Ads.csv')
+X= dataset.iloc[:,2:4].values
+Y= dataset.iloc[:,4].values
+
+#Splitting dataset
+from sklearn.cross_validation import train_test_split
+X_train, X_test, Y_train, Y_test = train_test_split(X,Y, test_size= 0.25, random_state=0)
+
+#Scaling data
+from sklearn.preprocessing import StandardScaler
+sc_X=StandardScaler()
+X_train=sc_X.fit_transform(X_train)
+X_test=sc_X.fit_transform(X_test)
+
+#Classifier
+
+#Prediction
+Y_pred= classifier.predict(X_test)
+
+#Confusion Matrix
+from sklearn.metrics import confusion_matrix
+cm=confusion_matrix(Y_test,Y_pred)
+
+#Visualising the results
+from matplotlib.colors import ListedColormap
+X_set,Y_set=X_train,Y_train
+X1,X2 = np.meshgrid(np.arange(start=X_set[:,0].min()-1, stop=X_set[:,0].max()+1, step=0.01), 
+                    np.arange(start=X_set[:,1].min()-1, stop=X_set[:,1].max()+1, step=0.01))
+plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(),X2.ravel()]).T).reshape(X1.shape),
+                    alpha=0.75, cmap=ListedColormap(('red','green')))
+plt.xlim(X1.min(),X1.max())
+plt.ylim(X2.min(),X2.max())
+for i,j in enumerate (np.unique(Y_set)):
+    plt.scatter(X_set[Y_set == j, 0], X_set[Y_set == j, 1],
+                c = ListedColormap(('red', 'green'))(i), label = j)
+plt.title('Classifier (Training set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.show()
+    
+
+X_set,Y_set=X_test,Y_test
+X1,X2 = np.meshgrid(np.arange(start=X_set[:,0].min()-1, stop=X_set[:,0].max()+1, step=0.01), 
+                    np.arange(start=X_set[:,1].min()-1, stop=X_set[:,1].max()+1, step=0.01))
+plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(),X2.ravel()]).T).reshape(X1.shape),
+                    alpha=0.75, cmap=ListedColormap(('red','green')))
+plt.xlim(X1.min(),X1.max())
+plt.ylim(X2.min(),X2.max())
+for i,j in enumerate (np.unique(Y_set)):
+    plt.scatter(X_set[Y_set == j, 0], X_set[Y_set == j, 1],
+                c = ListedColormap(('red', 'green'))(i), label = j)
+plt.title('Classifier (Test set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.show()
+    
